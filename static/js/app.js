04045a0fcf0971b0b1eab609c5f5767eff535075
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-12-14 16:26:14 -0500

    Logic to select all required HTML elements

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+/***************************************************
+SELECT ALL REQUIRED HTML ELEMENTS
+****************************************************/
+
+// select the table body
+var tableBody = d3.select("tbody");
+
+// Select the dropdown elements
+var shapeDropDown = d3.select("#shape-select");
+var countryDropDown = d3.select("#country-select");
+var stateDropDown = d3.select("#state-select");
+var cityDropDown = d3.select("#city-select");
+
+// Select the input elements
+var inputDate = d3.select("#datetime");
+var inputCity = d3.select("#city");
+var inputState = d3.select("#state");
+
+// Select the buttons
+var button = d3.select("#filter-btn");
+var clearButton = d3.select("#clear-filter-btn");
+
+// Select the form
+var form = d3.select("form");
