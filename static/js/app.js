8520d6272f8305271b6a53cbd4a2614593b0ce74
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-12-14 16:30:20 -0500

    Function to filter data based on the filter search criteria provided by user

--- static/js/app.js
+++ static/js/app.js
@@ -47,3 +47,46 @@ function loadTableData(ufoSightingsArray) {
         })
     }
 };
+
+// function to complete the event handler function for the filter button
+function filterData() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Get the value property of the input elements
+    var inputDateValue = inputDate.property("value");
+    var inputCountryValue = countryDropDown.property("value");
+    var inputStateValue = stateDropDown.property("value");
+    var inputCityValue = cityDropDown.property("value");
+    var inputShapeValue = shapeDropDown.property("value");
+
+    // filter the table data based on the specified filter criteria 
+    var filteredData = tableData;
+    if (inputDateValue != "") {
+        filteredData = filteredData.filter(ufoSighting => ufoSighting.datetime === inputDateValue);
+        console.log("Filtered by Date");
+    }
+    if (inputCityValue != "" && filteredData.length > 0) {
+        filteredData = filteredData.filter(ufoSighting => ufoSighting.city === inputCityValue);
+        console.log("Filtered by City");
+    }
+    if (inputStateValue != "" && filteredData.length > 0) {
+        filteredData = filteredData.filter(ufoSighting => ufoSighting.state === inputStateValue);
+        console.log("Filtered by State");
+    }
+    if (inputCountryValue != "" && filteredData.length > 0) {
+        filteredData = filteredData.filter(ufoSighting => ufoSighting.country === inputCountryValue);
+        console.log("Filtered by Country");
+    }
+    if (inputShapeValue != "" && filteredData.length > 0) {
+        filteredData = filteredData.filter(ufoSighting => ufoSighting.shape === inputShapeValue);
+        console.log("Filtered by Shape");
+    }
+
+    // remove all rows from the table body
+    tableBody.html("");
+
+    // load the table body with filtered data
+    loadTableData(filteredData);
+};
